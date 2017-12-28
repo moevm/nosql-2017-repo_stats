c0910d132a023fde6f7db995c63d82aa3894dc14
Milana Nazar <milana.nazarenkova@mail.ru>
2017-12-28 13:21:55 +0300

    version 1.0

--- Gui.java
+++ Gui.java
@@ -28,7 +28,13 @@ public class Gui extends JFrame {
     private JButton r_button = new JButton("Repo's");
     private JButton e_button = new JButton("Events");
     private JButton a_button = new JButton("Actors");
+    private JButton stat_button = new JButton("Statistic");
+    private JButton top_button = new JButton("Top Repositories");
+    private JButton[] lang_buttons = new JButton[10];
     private static nPanel graph = new nPanel();
+    private static JPanel statPane;
+    private static JPanel objPane;
+    private static JPanel langPane;
  
     private static MongoDatabase db;
     private static MongoCollection <Document> col;
@@ -42,18 +48,51 @@ public class Gui extends JFrame {
         this.setBounds(100,100,900,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container container = this.getContentPane();
-            container.add(graph,BorderLayout.CENTER);
-            JPanel jPane = new JPanel();
-            container.add(jPane,BorderLayout.PAGE_END);
-        jPane.add(r_button);
-        jPane.add(e_button);
-        jPane.add(a_button);
+          container.add(graph,BorderLayout.CENTER);
+          JPanel buttPane = new JPanel(new BorderLayout());
+          container.add(buttPane,BorderLayout.PAGE_END);
+          statPane = new JPanel();
+          buttPane.add(statPane,BorderLayout.PAGE_START);
+          langPane = new JPanel();
+          buttPane.add(langPane,BorderLayout.PAGE_END);
+          objPane = new JPanel();
+          container.add(objPane,BorderLayout.PAGE_START);
+        for (int j = 0;j<10;j++)
+        {
+          lang_buttons[j] = new JButton(Langs[j]);
+          langPane.add(lang_buttons[j]);
+          lang_buttons[j].addActionListener(new ButtonEventListener());
+          lang_buttons[j].setActionCommand(Integer.toString(j+3));
+        }
+        statPane.add(r_button);
+        statPane.add(e_button);
+        statPane.add(a_button);
+        objPane.add(stat_button);
+        objPane.add(top_button);
+        stat_button.addActionListener(new ButtonEventListener());
+        stat_button.setActionCommand("stat");
+        top_button.addActionListener(new ButtonEventListener());
+        top_button.setActionCommand("top");
         r_button.addActionListener(new ButtonEventListener());
 	r_button.setActionCommand("0");
 	e_button.addActionListener(new ButtonEventListener());
 	e_button.setActionCommand("1");
  	a_button.addActionListener(new ButtonEventListener());
 	a_button.setActionCommand("2");
+    }
+
+    private static String[] getTop(String lang, String[] repos, int[] events)
+    {
+      String[] ret = new String[10];
+      MongoCursor<Document> cur = col.find(eq("langs",lang)).sort(descending("events")).iterator();
+      for (int i=0;i<10;i++)
+      {
+        Document doc = cur.next();
+        ret[i] = doc.get("repository")+":"+doc.get("events");
+        repos[i] = doc.get("repository").toString();
+        events[i] = (int)doc.get("events");
+      } 
+      return ret;
     }
  
     private static int[] getStats(String lang)
@@ -77,8 +116,24 @@ public class Gui extends JFrame {
 	  String action = e.getActionCommand();
 	  //System.out.println("Button clicked! Command is " + action);
             try{
+                  graph.setVisible(true);
 		  graph.mode = Integer.parseInt(action);
-		} catch (Throwable t) {System.out.println("Parse Error!"+action);}
+		} catch (Throwable t) 
+            {
+              if (action.equals("stat"))
+              {
+                graph.setVisible(false);
+                langPane.setVisible(false);
+                statPane.setVisible(true);
+              }
+              if (action.equals("top"))
+              {
+                graph.setVisible(false);
+                statPane.setVisible(false);
+                langPane.setVisible(true);
+              }
+
+            }
 	    app.getContentPane().validate();
 	    app.getContentPane().repaint();  
         }
@@ -94,23 +149,37 @@ public static void main(String[] args) {
     {
       int[] stat = getStats(Langs[i]);
       graph.map.put(Langs[i],stat);
+      int[] events = new int[10];
+      String[] repos = new String[10];
+      getTop(Langs[i],repos,events);
+      graph.topRepo.put(Langs[i],repos);
+      graph.topCount.put(Langs[i],events);
     }
   app = new Gui();
+  graph.setVisible(false);
+  statPane.setVisible(false);
+  langPane.setVisible(false);
   app.setVisible(true);
+  
   }
 }
+
  
 class nPanel extends JPanel {
     public Map<String, int[]> map = new HashMap<String, int[]>();
+    public Map<String, String[]> topRepo = new HashMap<String, String[]>();
+    public Map<String, int[]> topCount = new HashMap<String, int[]>();
     public String[] keys;
     public Integer mode;
     private Integer[] norms = {25,100,40};
-    private String[] text = {"Repositoreies count on github for 01.01.2016",
-			"Events performed count on github for 01.01.2016",
-			"User's count, used github for 01.01.2016"};
+    private String[] text = {
+        "Repositoreies count on github for 01.01.2016",
+	"Events performed count on github for 01.01.2016",
+	"User's count, used github for 01.01.2016",
+        "TOP-20 Repositories for "};
  
     public nPanel() {
-        setBorder(BorderFactory.createLineBorder(Color.black));
+        //setBorder(BorderFactory.createLineBorder(Color.black));
 	mode = -1;
     }
  
@@ -120,21 +189,42 @@ class nPanel extends JPanel {
  
     public void paintComponent(Graphics g) {
         super.paintComponent(g);    
+        //System.out.println(mode);
         // System.out.println(getWidth()+","+getHeight());  
         // Draw Text
+        int x = getWidth()/32;
+        int h = getHeight()/24;
+        float f;
+        if (x<h) f = x;
+        else f = h;
+        f = f*3/4;
+        g.setFont(g.getFont().deriveFont(f));
+        int max = 0;
         if ((mode>=0) && (mode<=2)){
-        for (int i=0;i<keys.length;i++){
-          int y = 100+i*40;
-          int wid = 10;
-          int val = map.get(keys[i])[mode];
-	  g.drawString(text[mode],200,60);
-          g.drawString(keys[i],100,y);
-          g.drawString(Integer.toString(val),250,y);
-          g.setColor(Color.RED);
-          g.fillRect(350,y-wid,val / norms[mode],wid*2);
-          g.setColor(Color.BLACK);
-          g.drawRect(350,y-wid,val / norms[mode],wid*2);
+	  g.drawString(text[mode],x*6,h);
+          for (int i=0;i<keys.length;i++){
+            int y = h*3+i*h*2;
+            int val = map.get(keys[i])[mode];
+            if (i==0) max = val;
+            g.drawString(keys[i],x*4,y);
+            g.drawString(Integer.toString(val),x*8,y);
+            g.setColor(Color.RED);
+            g.fillRect(x*10,y-h,val*20*x / max,h);
+            g.setColor(Color.BLACK);
+            g.drawRect(x*10,y-h,val*20*x / max,h);
+          }
         }
+        if ((mode>=3) && (mode <=12))
+        {
+	  g.drawString("TOP-10 repositories for "+keys[mode-3],x*6,h);
+          for (int i=0;i<10;i++)
+          {
+            int y = h*3+i*h*2;
+            g.drawString(Integer.toString(i+1),x*2, y);
+            g.drawString(topRepo.get(keys[mode-3])[i],x*4, y);
+            g.drawString(Integer.toString(topCount.get(keys[mode-3])[i]),x*28, y);
+         
+          }
 	}
     }  
 }
